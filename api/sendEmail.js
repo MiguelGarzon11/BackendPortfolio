708df8ac1aba52e150699f4a790f908e21d4c38f
Miguel Angel <miguelangelgarb@gmail.com>
2025-08-07 00:01:33 -0500

    Portfolio v.2.0

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,39 +1,35 @@
 import nodemailer from 'nodemailer';
 
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).send({ message: 'Método no permitido' });
+  }
 
-export default async function(req, res) {
-    if (req.method !== 'POST') {
-        return res.status(405).send({ message: 'Metodo no permitido' });
-    }
+  const { email, subject, message } = req.body;
 
-    const { email, subject, message } = req.body;
+  if (!email || !subject || !message) {
+    return res.status(400).json({ success: false, message: 'Faltan datos requeridos' });
+  }
 
-    if ( !email || !subject || !message ) {
-        return res.status(400).json({ success:false, message: 'Faltan datos requeridos' });
-    }
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.MY_EMAIL,
+      pass: process.env.MY_PASS,
+    },
+  });
 
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.MY_EMAIL,
-            pass: process.env.MY_PASS,
-        },
-    });
+  const mailOptions = {
+    from: email,
+    to: process.env.MY_EMAIL,
+    subject,
+    text: message,
+  };
 
-    const mailOptions = {
-        from: email,
-        to: process.env.MY_EMAIL,
-        subject,
-        text: message,
-    };
-
-    try {
-        await transporter.sendMail(mailOptions);
-        return res.status(200).json({ success: true, message: 'Correo enviado con éxito' });
-    }
-
-    catch (err) {
-        return res.status(500).json({ success: false, message: err.message });
-    };
-    
+  try {
+    await transporter.sendMail(mailOptions);
+    return res.status(200).json({ success: true, message: 'Correo enviado con éxito' });
+  } catch (err) {
+    return res.status(500).json({ success: false, message: err.message });
+  }
 }
